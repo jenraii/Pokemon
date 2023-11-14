94e40ffff153a9a01640a2405ffaa0a850f72cd7
jenraii <131862365+jenraii@users.noreply.github.com>
2023-11-14 18:17:41 +0200

    PokemonRepository Query

--- src/main/java/s23/PokemonDatabase/domain/PokemonRepository.java
+++ src/main/java/s23/PokemonDatabase/domain/PokemonRepository.java
@@ -2,12 +2,20 @@ package s23.PokemonDatabase.domain;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 
 public interface PokemonRepository extends CrudRepository <Pokemon, Long> {
 	
+	 @Query(value = "select * from pokemon p where p.name like %:keyword% or p.type like %:keyword% or p.region like %:keyword% "
+	 		+ "or p.trainerid like %:keyword%", nativeQuery = true)
+	 
+	 List<Pokemon> findByKeyword(@Param("keyword") String keyword);
+	 
 	 List<Pokemon> findByName(String name);
+	 
 	 List<Pokemon> findByTrainerName(String string);
 	 List<Pokemon> findByType (String string);
 	 
